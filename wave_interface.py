974367593f1fcaf5e5ba84160d09b0ee7858b68b
haoqizhenhao <328187575@qq.com>
2018-10-30 19:41:38 +0800

    Update wave_interface.py

--- wave_interface.py
+++ wave_interface.py
@@ -5,7 +5,7 @@ Created on Mon Oct 29 10:32:02 2018
 @author: haoqi
 """
 
-from main import *
+import main
 
 #filename = 'data/取样数据1.txt'
 
@@ -56,25 +56,9 @@ def interface_txt(filename = 'data/取样数据1.txt'):
     
     np.savetxt("features.txt", features,fmt='%s') #保存为txt文件
     
+    dic = features_dict(wave,loc_peak, y_peak, loc_valley, y_valley)
+    dict_values = values(dic)
+    
+    np.savetxt("features_values.txt", [dict_values[i] for i in dict_values],fmt='%s') #保存为txt文件
+    
     return
-
-def interface1(filename = 'data/取样数据1.txt'):
-    pc = loaddata(filename)
-    pc = normalization(pc)
-    
-    b,a = signal.butter(3,0.009,'high')
-    sf = signal.filtfilt(b,a,pc)
-    b,a = signal.butter(3,0.1,'low')
-    pc1 = signal.filtfilt(b,a,sf)
-    
-    wave = wave_average(pc1)
-    
-    wave_length = len(wave)
-    
-    T = wave_T(fs = 200, length = wave_length)
-    loc_peak, y_peak, loc_valley, y_valley = find_features(wave)
-    
-    loc_peak, y_peak, loc_valley, y_valley = features_choose(wave,loc_peak, y_peak, loc_valley, y_valley)
-    get_figure(wave,loc_peak, y_peak, loc_valley, y_valley)
-    
-    features = [wave,T,loc_peak, y_peak, loc_valley, y_valley]
